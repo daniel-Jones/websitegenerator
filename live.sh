ba45e82c7881dcabdc8149ebaca42b1df55914bc
Daniel Jones <admin@danieljon.es>
2020-05-26 00:24:22 +0930

    update live script for new server

--- live.sh
+++ live.sh
@@ -5,6 +5,7 @@ git pull
 make clean
 make
 ./generate
-cp -r output/* /var/www/html
-chmod -R +r /var/www/html/blog
-chmod -R +r /var/www/html/posts
+cp -r output/* /var/www/danieljon.es
+setfacl -Rm u::rwX,g::r-X,o::--- /var/www/danieljon.es/
+setfacl -Rdm u::rwX,g::r-X,o::--- /var/www/danieljon.es/
+restorecon -vRF /var/www/danieljon.es
